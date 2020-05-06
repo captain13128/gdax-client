5ae952582d5922a0dbd0930e14deb3eaec958144
captain13128 <capitan.develop@gmail.com>
2020-05-06 18:38:09 +0300

    add setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='gdax-client',
+    version='1.0.0',
+    packages=[
+        'gdax_client',
+    ],
+    url='https://github.com/captain13128/chief-keeper',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    install_requires=[
+        "websocket-client==0.46.0",
+        "sortedcontainers==2.1.0",
+    ]
+)
